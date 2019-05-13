47df806d2a4d8d0c36d72396976890dbc70adb8d
alranna <sleepeatcoderepeat@gmail.com>
2019-05-13 18:44:11 +0500

    Аргументы мэйна с новой строки

--- src/main/java/hw/Hw.java
+++ src/main/java/hw/Hw.java
@@ -5,8 +5,9 @@ import java.lang.reflect.Array;
 public class Hw {
     public static void main(String[] args) {
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < Array.getLength(args); i++)
-            builder.append(args[i]);
+        for (int i = 0; i < args.length; i++) {
+            builder.append("\n" + args[i]);
+        }
         System.out.println("Hello world!" + builder.toString());
     }
 
