802d33ebee879fbd6922fd1f97d2ff6a9c88203b
alranna <sleepeatcoderepeat@gmail.com>
2019-05-13 16:53:59 +0500

    Проверка консоли контроля версий

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 SET JAVA_HOME=C:\Program Files\Java\jdk-12.0.1
-mvn clean install exec:java
+mvn clean compile exec:java  -Dexec.args="' Have a good day!'"
 pause
--- src/main/java/hw/Hw.java
+++ src/main/java/hw/Hw.java
@@ -1,8 +1,14 @@
 package hw;
 
+import java.lang.reflect.Array;
+
 public class Hw {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < Array.getLength(args); i++)
+            builder.append(args[i]);
+        System.out.println(builder.toString());
+        System.out.println("Hello world!" + builder.toString());
     }
 
 }
